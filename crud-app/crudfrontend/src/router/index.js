86dacc7c41102907416c974beb2128a34341932a
Yamazaki-Khaio <kfonseca@ecomp.uefs.br>
2023-08-09 23:38:17 -0300

    init

--- crud-app/crudfrontend/src/router/index.js
+++ crud-app/crudfrontend/src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import ListAddress from '@/components/ListAddress'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/ola',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },
+
+      {
+        path: '/',
+        name: 'ListAddress',
+        component: ListAddress
+      }
   ]
 })
